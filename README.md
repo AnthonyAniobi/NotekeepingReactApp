ab1e6959fb2aaa0e87923e7c60ea60b4defe1a10
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-23 19:56:50 +0100

    routing

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 ## A react project for storing and retrieving notes
 
-This application helps to make
+This application helps to  write notes and have them persist on the device. It also alarms the user when the time for a schedule is due
+
+#### libraries used
+- react-bootstrap
+- react-router-dom
+
+
+
+##### Attributions
+- Icon image by (Smashicons -Flaticon)[https://www.flaticon.com/free-icons/notes]
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// pages
+import Home from './pages/Home';
+import Notes from './pages/Notes';
+import Todo from './pages/Todo';
+import ErrorPage from './pages/ErrorPage';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='notes' element={<Notes />} />
+        <Route path='todo' element={<Todo />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
